1db4725d5ab1310113adf584d9a44d68e736f577
darkplanetar <burakdarkp@yahoo.com>
2022-06-06 16:28:48 +0300

    29.05.2022

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 import Skills from "./components/SkillCard";
-function App() {
+import React, { useRef } from "react";
+import { useInViewport } from "react-in-viewport";
+function App(props) {
+  const myRef = useRef();
+
+  const { inViewport, enterCount, leaveCount } = useInViewport(myRef, props);
+  console.log(inViewport, enterCount);
   return (
     <div>
       <Header></Header>
-      <Skills></Skills>
+      <AboutMe></AboutMe>
+      <div ref={myRef} style={{ height: "100vh" }}>
+        {inViewport && enterCount === 1 && <Skills></Skills>}
+      </div>
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./AboutMe.module.css";
+function AboutMe(props) {
+  return (
+    <div className={classes.parag}>
+      <h2 className={classes.h2Class}>ABOUT ME</h2>
+      <p>
+        Front-end developer who loves to keep tracking and learning new
+        technologies. My newborn daughter who adds more motivation to me to keep
+        me on track for working hard :). With a good educational background in
+        Turkey and twelve years expreience in this industry, I've honed my
+        analytical thinking and collaboration skills, and love working with a
+        team. Last 5 years I started focus on front-end development with my best
+        friend guidance, who he’s a master full-stack developer in England.
+      </p>
+      <img />
+      <img />
+      <img />
+    </div>
+  );
+}
+
+export default AboutMe;
