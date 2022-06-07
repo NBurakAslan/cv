0038b701a47a622f78f4c25b5681303e5e5993b7
darkplanetar <burakdarkp@yahoo.com>
2022-06-07 15:13:06 +0300

    07.06.2022

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,13 +8,14 @@ function AboutMe(props) {
     <div className={classes.parag}>
       <div className={classes.h2Class}>ABOUT ME</div>
       <p>
-        Front-end developer who loves to keep tracking and learning new
-        technologies. My newborn daughter who adds more motivation to me to keep
-        me on track for working hard :). With a good educational background in
+        Front-end developer who loves to keep track of and learn new
+        technologies. My newborn daughter adds more motivation for me to keep me
+        on track for working hard :). With a good educational background in
         Turkey and twelve years expreience in this industry, I've honed my
         analytical thinking and collaboration skills, and love working with a
-        team. Last 5 years I started focus on front-end development with my best
-        friend guidance, who he’s a master full-stack developer in England.
+        team. Last 5 years I started focusing on front-end development with my
+        best friend's guidance, who is an accomplished full-stack developer in
+        England.
       </p>
       <div className={classes.cardfan}>
         <img alt="My Daughter" src={lacin} />
